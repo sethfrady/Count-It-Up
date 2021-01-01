6157aa706a3f586272469fbbc754084ef11105a1
Seth Frady <sethfrady@outlook.com>
2020-12-31 22:53:24 -0500

    JS Counter

---
+++ script.js
@@ -0,0 +1,37 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(item) {
+    item.addEventListener("click", function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+            value.textContent = count;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+            value.textContent = count;
+        } else {
+            count = 0;
+            value.textContent = count;
+        }
+
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+
+        if (count < 0) {
+            value.style.color = 'red';
+        }
+
+        if (count === 0) {
+            value.style.color = 'blue';
+        }
+    })
+})
+
+
